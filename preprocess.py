3679bf47ffb9ceb2f9f0502c6b113a5441cc74c7
sahaj-96 <sahajsrivastava96@gmail.com>
2024-04-13 17:35:14 +0530

    added frameskipper class

--- preprocess.py
+++ preprocess.py
@@ -26,7 +26,7 @@ class EpisodeEnv(gym.Wrapper):
             observ, _, _, _ = self.env.step(0)    
         return observ
 
-class SclaeRwrd(gym.RewardWrapper):
+class ScaleRwrd(gym.RewardWrapper):
     def rwrd(self, rwrd):
 
         return rwrd * 0.1#ensures reward within a spec range for PPO 
@@ -45,3 +45,37 @@ class pre_process(gym.ObservationWrapper):
 
         return frame
 
+class frameskipper(gym.Wrapper):
+    def __init__(self, env, nofframes, stickprob):
+        gym.Wrapper.__init__(self, env)
+        self.nofframes = nofframes
+        self.stickprob = stickprob
+        self.curr_action = None
+        self.rng = np.random.RandomState()
+        self.supports_want_render = hasattr(env, "supports_want_render")#flag i dicates whether env should render itself or not
+
+    def reset(self, **kwargs):
+        self.curr_action = None
+        return self.env.reset(**kwargs)# reset wrapped env to its initial state and return initial obv
+
+    def step(self, action):
+        done = False
+        total_reward = 0
+        for i in range(self.nofframes):
+            if self.curr_action is None:#if first step
+                self.curr_action = action
+            elif i==0:# first substep of a frame
+                if self.rng.rand() > self.stickprob:#to add stochasticity by occasionally sticking with same action.
+                    self.curr_action = action
+            elif i==1:
+                self.curr_action = action
+            if self.supports_want_render and i<self.nofframes-1:
+                observ, rew, done, info = self.env.step(self.curr_action, want_render=False)
+            else:
+                observ, rew, done, info = self.env.step(self.curr_action)
+            total_reward += rew
+            if done: break
+        return observ, total_reward, done, info
+
+    def seed(self, s):
+        self.rng.seed(s) 
