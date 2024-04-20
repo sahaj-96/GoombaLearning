b338cad32861b3cd57ded91f1c31d674fc781c93
sahaj-96 <sahajsrivastava96@gmail.com>
2024-04-20 23:24:37 +0530

    Added class for controlling the env

--- environments.py
+++ environments.py
@@ -1,4 +1,33 @@
-class TimeOrderedList(object):
+from multiprocessing import Pipe,Process
+
+class Envcontrol:
+    def controller(name,connection):
+        while True:
+            (command,args,kwargs)=connection.recv()
+            if command=="reset":
+                connection.send(env.reset())
+            elif command=="step":
+                connection.send(env.step(*args,**kwargs))
+            elif command=="exit":
+                break
+            else:
+                raise Exception(f"Unknown command {command}")
+    def __init__(self,n):
+        parent_side,child_side=Pipe()
+        self.process=Process(target=Envcontrol.controller,args=(n,child_side))
+        self.process.start()
+        self.connection=parent_side
+    def reset(self):
+        self.connection.send(("reset",(),{}))
+        return self.connection.recv()
+    def step(self, action):
+        self.connection.send(("step",(action,),{}))
+        return self.connection.recv()
+    def exit(self):
+        self.connection.send(("exit",(),{}))
+        self.process.join()
+
+class TimeOrderedList:
     def __init__(self,first_time=0):
         self.first_time=first_time
         self.list=[]
