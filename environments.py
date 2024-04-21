0134d04c3e31cab68680c380537f81a710744476
sahaj-96 <sahajsrivastava96@gmail.com>
2024-04-21 13:08:05 +0530

    Added class main controller with basic step func method

--- environments.py
+++ environments.py
@@ -1,5 +1,9 @@
 from multiprocessing import Pipe,Process
-
+import tensorflow as tf
+import numpy as np
+starting_time=0
+number_of_actions=-1 #TODO
+gamma=.9#TODO
 class Envcontrol:
     def controller(name,connection):
         while True:
@@ -42,6 +46,47 @@ class TimeOrderedList:
         return self.list[at_time_t-self.first_time]
     def future_len(self):
         return len(self.list)
-
     def in_range(self,from_time,length):
         return self.list[(from_time-self.first_time):(from_time-self.first_time+length)]
+
+class Maincontroller:
+    def __init__(self, environment):
+        self.env=environment
+        self.observation=TimeOrderedList(first_time=starting_time) 
+        self.last_observation=tf.convert_to_tensor(np.expand_dims(self.env.reset(),axis=0),dtype=tf.float32)
+        self.observation.append(self.last_observation)
+        self.act = self.rew = self.val = self.policy = self.delta = self.done = TimeOrderedList(first_time=starting_time)
+        self.episode_start_time = 0
+        self.rew_ofeach_episode = self.xpos_ofeach_episode = self.rew_ofcurr_episode = self.xpos_ofcurr_episode = []
+        self.estimate_the_advantages = self.estimate_the_values = TimeOrderedList(first_t=starting_time)
+
+    def take_step_in_env(self,p_nn,v_nn,t):      
+        if t==starting_time:
+            val_0=v_nn(self.last_observation).numpy()[0]
+            self.val.append(val_0[0])
+        policy_t=p_nn(self.last_obs).numpy()[0]
+        action_t=np.random.choice(number_of_actions,1,p=policy_t)[0] #1 because i need to draw a single action
+        next_state,step_reward,episode_done,step_info=self.env.step(action_t)        
+        next_state=np.expand_dims(next_state,axis=0)
+        self.act.append(action_t)
+        self.rew.append(step_reward)
+        self.policy.append(policy_t)
+        self.rew_ofcurr_episode.append(step_reward)        
+        self.xpos_ofcurr_episode.append(step_info.get('x_pos'))
+        if episode_done:
+            self.done.append(True)
+            self.rew_ofeach_episode.append(sum(self.rew_ofcurr_episode))
+            self.rew_ofcurr_episode.clear()
+            self.xpos_ofeach_episode.append(self.xpos_ofcurr_episode)
+            self.xpos_ofcurr_episode.clear()
+            next_state=self.env.reset()
+            next_state=np.expand_dims(next_state,axis=0)
+            self.episode_start_time=t+1
+        else:
+            self.done.append(False)
+        self.observation.append(next_state)      
+        next_state = tf.convert_to_tensor(next_state,dtype=tf.float32)
+        next_state_value_estimate = v_nn(next_state).numpy()[0] 
+        self.val.append(next_state_value_estimate[0])  
+        self.delta.append(self.rew.get(t) + (1 if episode_done else 0)*gamma*self.val.get(t+1)-self.val.get(t))
+        self.last_observation=next_state
