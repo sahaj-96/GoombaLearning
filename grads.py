bd38626ea3887a360320afeb35f713c2e519b58e
sahaj-96 <sahajsrivastava96@gmail.com>
2024-05-23 15:41:21 +0530

    Necessary fixes

--- grads.py
+++ grads.py
@@ -1,35 +1,37 @@
 import tensorflow as tf
 from . import consts
 maximum_gradient = consts.gradient_max
-number_of_actions = consts.num_actions
+num_actions = consts.num_actions
 base_clip_epsilon = consts.base_clip_epsilon
 value_loss_coefficient = consts.value_loss_coefficient
 entropy_loss_coefficient = consts.entropy_loss_coefficient
 
 @tf.function(experimental_relax_shapes=True)
-def loss(alpha,p_nn,v_nn,obv,actions,adv,old_p,old_v):    
-    policy=p_nn(obv)    
-    v=v_nn(obv)#compute values
-    v=tf.squeeze(v,axis=1)    
-    act_one_hot=tf.one_hot(actions,number_of_actions)
-    clip_epsilon=tf.math.multiply(alpha,base_clip_epsilon)
-    log_prob_r=tf.math.log(tf.reduce_sum(policy*act_one_hot, axis=1))-tf.math.log(tf.reduce_sum(old_p*act_one_hot, axis=1))                                   
-    prob_r=tf.math.exp(log_prob_r)  
-    clipped_prob_r=tf.clip_by_value(prob_r,1-clip_epsilon,1+clip_epsilon)
-    entropy_loss=-tf.reduce_sum(-policy*tf.math.log(policy),axis=1)   
-    clip_loss=-tf.math.minimum(prob_r*adv,clipped_prob_r*adv) 
-    value_loss=tf.math.square(v-old_v)        
-    total_loss=tf.reduce_mean(clip_loss+value_loss_coefficient*value_loss+entropy_loss_coefficient*entropy_loss)
-    return entropy_loss,clip_loss,value_loss,total_loss
+def loss(alpha,policy_network,value_network,observation,actions,advantages,old_policies,old_values):    
+    policy = policy_network(observation)    
+    v = value_network(observation)
+    v = tf.squeeze(v, axis = 1)    
+    act_one_hot = tf.one_hot(actions, num_actions)    
+    clip_epsilon = tf.math.multiply(alpha , base_clip_epsilon)
+    log_prob_ratio = tf.math.log(tf.reduce_sum(policy * act_one_hot, axis=1)) - tf.math.log(tf.reduce_sum(old_policies * act_one_hot, axis=1))                                   
+    prob_ratio = tf.math.exp(log_prob_ratio)             
+    clipped_prob_ratio = tf.clip_by_value(prob_ratio, 1 - clip_epsilon, 1 + clip_epsilon)                                            
+    entropy_loss = -tf.reduce_sum(- policy * tf.math.log(policy), axis=1)                        
+    clip_loss = -tf.math.minimum(prob_ratio * advantages, clipped_prob_ratio * advantages)                                         
+    value_loss = tf.math.square(v - old_values)        
+    total_loss = tf.reduce_mean(clip_loss + value_loss_coefficient * value_loss + entropy_loss_coefficient * entropy_loss)            
+    return entropy_loss, clip_loss, value_loss, total_loss
+
 
 def gradients(optimizer):  
-    @tf.function      #Dynamic Polymorphism
-    def grads(alpha,p_nn,v_nn,obv,actions,adv,old_p,old_v):
+    @tf.function
+    def apply_grads(alpha,policy_network,value_network,observation,actions,advantages,old_policies,old_values):
+        
         with tf.GradientTape() as tape:                  
-            entropy_loss,clip_loss,value_loss,total_loss=loss(alpha,p_nn,v_nn,obv,actions,adv,old_p,old_v)
-        all_variables=(p_nn.trainable_variables+v_nn.trainable_variables)
-        initial_grads=tape.gradient(total_loss,all_variables)
-        clipped_grads,_=tf.clip_by_global_norm(initial_grads,maximum_gradient)
-        optimizer.apply_gradients(zip(clipped_grads,all_variables))     
-        return entropy_loss,clip_loss,value_loss,total_loss
-    return grads
+            entropy_loss, clip_loss, value_loss, total_loss=loss(alpha,policy_network,value_network,observation,actions,advantages,old_policies,old_values)
+        variables = (policy_network.trainable_variables + value_network.trainable_variables)
+        gradients = tape.gradient(total_loss, variables)
+        clipped_gradients, _ = tf.clip_by_global_norm(gradients, maximum_gradient)
+        optimizer.apply_gradients(zip(clipped_gradients, variables))     
+        return entropy_loss, clip_loss, value_loss, total_loss
+    return apply_grads
